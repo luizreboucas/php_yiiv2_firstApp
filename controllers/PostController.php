57ca14f8d060b993243b71ed55325dd30f391ed4
luiz.reboucas <luiz.reboucas@hostweb.com.br>
2023-12-11 10:13:59 -0300

    alterações finais - cruds realizados

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -4,10 +4,31 @@
     use app\models\Post;
     use app\models\User;
     use Yii;
-    use yii\web\Response;
+use yii\filters\VerbFilter;
+use yii\web\Response;
 
     class PostController extends Controller
     {
+        public function beforeAction($action)
+        {
+            $this->enableCsrfValidation = false;
+            return parent::beforeAction($action);
+        }
+
+        public function behaviors()
+        {
+            return [
+                'verbs'=> [
+                    'class'=> VerbFilter::class,
+                    'actions'=> [
+                        'create-post'=> ['post'],
+                        'update-post'=> ['put']
+                    ]
+                ]
+            ];
+        }
+        
+
         public function actionIndex()
         {
             $posts = Post::find()->with('user')->orderBy('id')->all();
@@ -29,4 +50,60 @@
             Yii::$app->response->format = Response::FORMAT_JSON;
             return $result;
         }
+
+        public function actionGetPost($id)
+        {
+            $post = Post::findOne($id);
+            Yii::$app->response->format = Response::FORMAT_JSON;
+            return [
+                'id' => $post->id,
+                'content'=> $post->content,
+                'user' => User::findOne($post->user_id)
+            ];
+        }
+
+        public function actionCreatePost()
+        {
+            $requestJson = file_get_contents('php://input');
+            $body = json_decode($requestJson);
+            $post = new Post();
+            $post->content = $body->content;
+            $post->user_id = $body->user;
+            $post->save();
+            Yii::$app->response->format = Response::FORMAT_JSON;
+            return [
+                'message' => 'post criado',
+                'post' => $post
+            ];      
+        }
+
+        public function actionUpdatePost($id)
+        {
+            $post = Post::findOne($id);
+            $requestBodyJson = file_get_contents('php://input');
+            $requestBody = json_decode($requestBodyJson);
+            if(isset($requestBody->content))
+            {
+                $post->content = $requestBody->content;
+                $post->save();
+            }
+
+            Yii::$app->response->format = Response::FORMAT_JSON;
+            return [
+                'message'=> 'post atualizado com sucesso!',
+                'post' => $post
+            ];
+        }
+
+        public function actionDeletePost($id)
+        {
+            $post = Post::findOne($id);
+            $post->delete();
+            Yii::$app->response->format = Response::FORMAT_JSON;
+            return [
+                'message' => 'post deletado com sucesso!',
+                'post' => $post
+            ];
+        }
+        
     }
